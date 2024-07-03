8bb88318660c6bd58f19b983d4c8035a891688ba
Piyush023 <piyush.khurana@rario.com>
2024-07-03 15:36:02 +0530

    Added SignUp and Login Buttons and HomePage Section Titles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import Header from '@/components/Header/Header';
+import PlaylistItem from '@/components/PlaylistCard/PlaylistItem';
 import React from 'react';
 
 const Home = () => {
+  const PlaylistItemData = {
+    name: 'Liked Songs',
+    href: '/',
+    image: '/images/liked.png',
+  };
   return (
     <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
-      <Header>Header</Header>
+      <Header>
+        <div className='mb-2'>
+          <h1 className='text-white text-3xl font-semibold'>Welcome Back</h1>
+          <div className='grid gird-cols-1 sm:gird-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
+            <PlaylistItem PlaylistItemProps={PlaylistItemData} />
+          </div>
+        </div>
+      </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-2xl font-semibold'>Newest Songs</h1>
+        </div>
+      </div>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-2xl font-semibold'>List of Songs</h1>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/PlaylistCard/PlaylistItem.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { FaPlay } from 'react-icons/fa';
+
+export interface PlaylistItemProps {
+  name: string;
+  image: string;
+  href: string;
+}
+const PlaylistItem: React.FC<{ PlaylistItemProps: PlaylistItemProps }> = ({
+  PlaylistItemProps,
+}) => {
+  const router = useRouter();
+  const onClick = () => {
+    //Add auth check here
+    router.push(PlaylistItemProps.href);
+  };
+  return (
+    <button
+      onClick={onClick}
+      className='relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4'
+    >
+      <div className='relative min-h-[64px] min-w-[64px]'>
+        <Image
+          src={PlaylistItemProps.image}
+          fill
+          alt='Image'
+          className='object-cover'
+        />
+      </div>
+      <p className='font-medium truncate py-5'>{PlaylistItemProps.name}</p>
+      <div className='absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110'>
+        <FaPlay className='text-black' />
+      </div>
+    </button>
+  );
+};
+
+export default PlaylistItem;
