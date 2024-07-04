57bc535208266cd5d5d9852ff55f169bb18e0682
Piyush023 <piyush.khurana@rario.com>
2024-07-04 14:12:46 +0530

    Added Login nad Logout Functionality using Supabase and AuthModal Provider for Login and Toast Provider for showing Toast.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import SupabaseProvider from '../../providers/SupabaseProvider';
 import UserProvider from '../../providers/UserProvider';
+import ModalProvider from '../../providers/ModalProvider';
+import ToasterProvider from '../../providers/ToastProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
